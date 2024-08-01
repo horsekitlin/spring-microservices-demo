c68e24e4770c5659aedbe8180a7e340b89983f58
PassonoTW <82664528+passontw@users.noreply.github.com>
2024-08-01 23:44:04 +0800

    remove react-froend

--- react-frontend/src/actions/employeeActions.js
+++
@@ -1,6 +0,0 @@
-import types from 'constants/actionTypes';
-
-export const getEmployeeByIdAction = (payload) => ({
-  type: types.GET_EMPLYEE_BY_ID,
-  payload,
-});
--- react-frontend/src/apis/employeeResult.js
+++
@@ -1,4 +0,0 @@
-import server from ".";
-
-export const getEmployeeByIdResult = ({ payload }) =>
-    server.get(`employee-service/api/employees/${payload.employeeId}`);
